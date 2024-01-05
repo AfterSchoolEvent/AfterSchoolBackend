e3c04ad1b48d97acd8156f3bac046358fcefb1bb
EunKyung Lee <2dmsrud2002@naver.com>
2024-01-05 13:33:36 +0900

    Chore: 백엔드 환경 세팅

---
+++ src/main/java/com/banggong/afterschoolevent/AfterSchoolEventApplication.java
@@ -0,0 +1,13 @@
+package com.banggong.afterschoolevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterSchoolEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterSchoolEventApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/banggong/afterschoolevent/AfterSchoolEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banggong.afterschoolevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterSchoolEventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
